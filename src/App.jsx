141ea379c3c0b55228f8c5293f349080c967cb7c
jayachandranmurugesh <jayachandran3102@gmail.com>
2025-05-17 22:33:47 +0530

    Base skeleton added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Dashboard from './components/dashboard/Dashboard'
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="content">
+     <Dashboard></Dashboard>
+    </div>
+  );
+};
+
+export default App;
